271a7670053d6e68acd35d02c8f447cd2d0a3867
netanelch230 <netanelch230@gmail.com>
2020-06-09 15:58:30 +0300

    asdf

--- include/Enum.h
+++ include/Enum.h
@@ -1,6 +1,23 @@
 #pragma once
-
 const int colorAmount = 4;
+const int recSize = 50;
+const int maxTextFields = 4;
+// 4 groups is the maximum
+
+const int minGroupIndex = 0;
+const int maxGroupIndex = 3;
+
+const int maxHeadlines = 5;
+const int gameFormatOptions = 2;
+const int colorsOptions = 4;
+const int backgroundOptions = 2;
+
+//=======================================locations on menu=========================================
+const float playersBottonX = 300;
+const float playersBottonXRatio = 150;
+const float playersBottonY = 200;
+const float gameFormatX = 10;
+
 const size_t wormsLimit = 5;
 const auto NumOfLife = 150;
 const int WIDTH = 1200;
@@ -48,4 +65,33 @@ enum Icon
 	pass,
 	sheep,
 	whiteflag
+};
+
+
+enum font
+{
+	menu_font
+};
+enum color
+{
+	yellow,
+	blue,
+	red,
+	green
+};
+
+enum Pictures
+{
+	menuPic,
+	menuHeadline,
+	maxTextures
+};
+
+enum menuFields
+{
+	gameFormat,
+	gameBackground,
+	numberOfPlayers,
+	groupColor,
+	groupName
 };
--- src/Menu.cpp
+++ src/Menu.cpp
@@ -1,113 +1,197 @@
 #include "Menu.h"
-#include <iterator>     // std::distance
 #include <iostream>
 #include <string>
 #include <SFML/Graphics.hpp>
-
-
+#include <algorithm>
+#include <utility>
+#include <iterator>     // std::distance
 Menu::Menu()
 {
+	setPlayerTextField();
+	setTextFieldsHeadlines();
 	initializeColors();
+	initializeBackground();
+	initializeGameFormat();
+	LoadImages();
+	setOptions();
 }
-void Menu::run(sf::Texture& menuScreen, sf::RenderWindow& window)
+void Menu::run(sf::RenderWindow& window)
 {
-	m_sprite.setTexture(menuScreen);
 	while (window.isOpen())
 	{
 		window.clear();
-		window.draw(m_sprite);
-		for (auto i : m_groupColors)
-			window.draw(i);
-		intializedefinitions(window);
+		drawMenu(window);
 		window.display();
 		if (auto event = sf::Event{}; window.waitEvent(event))
 		{
 			switch (event.type)
 			{
+			case sf::Event::TextEntered:
+				if (sf::Keyboard::isKeyPressed(sf::Keyboard::Enter))
+				{
+					m_currGroup++;
+				}
+				else
+				{
+					if (m_currGroup <= m_groupAmount)
+					{
+						m_playerInput[m_currGroup] += event.text.unicode;
+						m_playerText[m_currGroup].setString(m_playerInput[m_currGroup]);
+					}
+				}
+				break;
 				case sf::Event::Closed:
 					window.close();
 					break;
 				case sf::Event::MouseButtonReleased:
 				{
-					const auto location = window.mapPixelToCoords({ event.mouseButton.x, event.mouseButton.y });
-					/*switch (event.mouseButton.button)
+					auto location = window.mapPixelToCoords(
+						{ event.mouseButton.x, event.mouseButton.y });
+					switch (event.mouseButton.button)
 					{
 					case sf::Mouse::Button::Left:
-					case sf::Mouse::Button::Right:
-					}*/
-					if (m_playerText.getLocalBounds().contains(location))
-					{
-						while (sf::Event::TextEntered)
+						updatePress(location);
+						/*if (pressed == t_end)
 						{
-							m_playerInput += static_cast<char>(event.text.unicode);
-							m_playerText.setString(m_playerInput);
-							window.draw(m_playerText);
-							window.display();
+							window.close();
 						}
+						continue to play
+						*/
 					}
-					break;
 				}
-
 			}
 		}
 	}
 }
+
+void Menu::updatePress(sf::Vector2f location)
+{
+	for (auto i = 0; i < m_playersSprite.size(); i++)
+	{
+		if (m_playersSprite[i].getGlobalBounds().contains(location.x, location.y))
+		{
+			m_groupAmount = i;
+			for (int j = 0; j <= i; j++)
+			{
+				m_textFields[j].setFillColor(sf::Color::White);
+			}
+			break;
+		}
+	}
+	if (m_background1.getGlobalBounds().contains(location.x, location.y))
+		values.m_background= 1;
+
+	if (m_background2.getGlobalBounds().contains(location.x, location.y))
+		values.m_background = 2;
+		
+	if (m_playWithComp.getGlobalBounds().contains(location.x, location.y))
+		values.m_gameFormat = true;
+
+	if (m_playWithFriend.getGlobalBounds().contains(location.x, location.y))
+		values.m_gameFormat = false;
+
+}
+
 void Menu::initializeColors()
 {
-	m_groupColors.resize(colorAmount);
+	
+	m_headLines[groupColor].setPosition(300, 580);
+
+	m_groupColors.resize(colorsOptions);
 	for (auto c = m_groupColors.begin(); c!= m_groupColors.end(); ++c)
 	{
 		auto currLocation = std::distance(m_groupColors.begin(), c);
 		c->setFillColor(getColor(currLocation));
-		c->setSize(sf::Vector2f{ 50,50 });
-		c->setPosition((70* currLocation), 630);
+		c->setSize(sf::Vector2f{ 75 ,75 });
+		c->setPosition((300 + 100 * currLocation), 620);
 		c->setOutlineColor(sf::Color::Black);
 	}
 }
 
-void Menu::intializedefinitions(sf::RenderWindow& window)
+void Menu::setPlayerTextField()
 {
-	std::vector<std::string> menuOptions;
-	createTextOptions(menuOptions);
-	initializeGameFormat(menuOptions[0], window);
-	initializeColors();
-	
+	m_playerInput.resize(maxTextFields);
+	m_playerText.resize(maxTextFields);
+
+	for (auto i = 0; i < maxTextFields; i++)
+	{
+		m_textFields.push_back(createRectangleShape(i));
+	}
+
+	int j = 0;
+	for (auto i = 0; i < m_playerText.size(); i++)
+	{
+		m_playerText[i].setCharacterSize(25);
+		m_playerText[i].setFillColor(sf::Color::Black);
+		m_playerText[i].setStyle(sf::Text::Bold);
+		m_playerText[i].setOrigin(0, 10);
+		m_playerText[i].setPosition(500, 350+j);
+		j += 70;
+	}
+
 }
 
-void Menu::initializeGameFormat(std::string curr, sf::RenderWindow& window)
+sf::RectangleShape Menu::createRectangleShape(int row)
 {
 	sf::RectangleShape rec;
-	rec.setFillColor(sf::Color::White);
-	rec.setSize(sf::Vector2f{ 50,50 });
-	rec.setPosition(100, 100);
-	m_playerText.setCharacterSize(25);
-	m_playerText.setFillColor(sf::Color::Black);
-	m_playerText.setStyle(sf::Text::Bold);
-	m_playerText.setOrigin(0, 10);
-	m_playerText.setPosition(100, 100);
-	window.draw(rec);
-	window.draw(m_playerText);
-
-	m_headLine.setString(curr);
-	m_headLine.setCharacterSize(20);
-	m_headLine.setFillColor(sf::Color::Black);
-	m_headLine.setStyle(sf::Text::Bold);
-	m_headLine.setOrigin(0, 10);
-	m_headLine.setPosition(10, 10);
-	window.draw(m_headLine);
-	
-	// 2 options
-	m_gameFormat.resize(2);
-	for (auto c = m_gameFormat.begin(); c!= m_gameFormat.end(); ++c)
-	{
-		auto currLocation = std::distance(m_gameFormat.begin(), c);
-		c->setFillColor(sf::Color::Black);
-		c->setSize(sf::Vector2f{ 50,50 });
-		c->setPosition((70* currLocation), 30);
-		c->setOutlineColor(sf::Color::White);
-	}
+	rec.setFillColor(sf::Color::Transparent);
+	rec.setSize(sf::Vector2f{ 150, 50 });
+	rec.setPosition(500, 330+(row*70));
+	return rec;
 }
 
+void Menu::drawMenu(sf::RenderWindow& window)
+{
+	window.draw(m_spriteBackGround);
+	window.draw(m_spriteHeadline);
+	window.draw(m_playWithComp);
+	window.draw(m_playWithFriend);
+
+	for (auto i : m_playersSprite)
+		window.draw(i);
+	for (auto i : m_groupColors)
+		window.draw(i);
+	
+	for (auto i : m_background)
+		window.draw(i);
+
+	//for (auto i : m_gameFormat)
+		//window.draw(i);
+	
+
+	for (auto i = 0; i <= m_groupAmount; i++)
+	{
+		window.draw(m_textFields[i]);
+	}
+
+	for (auto i = 0 ; i < m_playerText.size(); ++i)
+	{
+		window.draw(m_playerText[i]);
+	}
+
+	//draw all head lines!
+	for (auto i : m_headLines)
+		window.draw(i);
+
+}
+
+void Menu::LoadImages()
+{
+	sf::Texture current;
+	current.loadFromFile("PlayWithFriends.png");
+	m_MenuTextures.push_back(current);
+	current.loadFromFile("PlayWithComputer.png");
+	m_MenuTextures.push_back(current);
+
+}
+
+void Menu::setOptions()
+{
+	m_playWithComp.setTexture(m_MenuTextures[0]);
+	m_playWithComp.setPosition(10, 200);
+	m_playWithFriend.setTexture(m_MenuTextures[1]);
+	m_playWithFriend.setPosition(10, 400);
+}
 
 sf::Color Menu::getColor(int colorNum)
 {
@@ -124,20 +208,70 @@ sf::Color Menu::getColor(int colorNum)
 	}
 }
 
-void Menu::setFont(sf::Font& font)
+void Menu::setResources(sf::Font& font, sf::Texture& backGround, sf::Texture& headLine,
+	std::vector<sf::Texture>& players) 
 {
-	m_headLine.setFont(font);
+	for (auto i = 0; i < m_headLines.size(); i++)
+		m_headLines[i].setFont(font);
+	
+	for (auto i = 0 ; i<m_playerText.size(); ++i)
+		m_playerText[i].setFont(font);
+
+	m_spriteBackGround.setTexture(backGround);
+	m_spriteHeadline.setTexture(headLine);
+	m_spriteHeadline.setPosition(250, 0);
+
+	sf::Sprite s;
+	m_playersSprite.resize(players.size());
+	for (auto i = 0; i < players.size(); i++)
+	{
+		m_playersSprite[i].setTexture(players[i]);
+		m_playersSprite[i].setPosition(playersBottonX +(i* playersBottonXRatio), playersBottonY);
+	}
+}
+
+void Menu::setTextFieldsHeadlines()
+{
+	m_headLines.resize(maxHeadlines);
+	m_headLines[gameFormat].setPosition(gameFormatX, 150);
+
+	int currHeadLine = 0;
+	setHeadLine(currHeadLine++, "Choose game format", 0, 0);
+	m_headLines[gameFormat].setPosition(10, 150);
+	setHeadLine(currHeadLine++, "Choose game background", 0, 0);
+	setHeadLine(currHeadLine++, "Choose number of players", 0, 0);
+	setHeadLine(currHeadLine++, "Choose game color", 0, 0);
+	setHeadLine(currHeadLine++, "Enter group name", 0, 0);
 }
 
 
-void Menu::createTextOptions(std::vector<std::string>& menuOptions)
+void Menu::initializeBackground()
 {
-	std::string curr = "choose the game format: ......."; // sapir
-	menuOptions.push_back(curr);
-	curr = "choose the game background"; // sapir
-	menuOptions.push_back(curr);
-	curr = "choose number of players"; // noa - numberOfPlayers+ names.x
-	menuOptions.push_back(curr);
-	curr = "choose color:"; // sapir
-	menuOptions.push_back(curr);
+	m_headLines[gameBackground].setPosition(900, 150);
+	//m_playWithComp.setTexture()
+	m_background.resize(backgroundOptions);
+	for (auto c = m_background.begin(); c!= m_background.end(); ++c)
+	{
+		auto currLocation = std::distance(m_background.begin(), c);
+		c->setFillColor(getColor(currLocation));
+		c->setSize(sf::Vector2f{ 250,200 });
+		c->setPosition(900 , 200 + (200*currLocation));
+		c->setOutlineColor(sf::Color::Black);
+	}
+}
+
+
+void Menu::setHeadLine(const int index, std::string headLine, float x, float y)
+{
+	m_headLines[index].setString(headLine);
+	m_headLines[index].setCharacterSize(30);
+	m_headLines[index].setFillColor(sf::Color::Black);
+	m_headLines[index].setStyle(sf::Text::Bold);
+	m_headLines[index].setOrigin(0, 10);
+	m_headLines[index].setPosition(10, 30*(index+10));
+}
+
+void Menu::initializeGameFormat()
+{
+	m_headLines[gameFormat].setPosition(10, 150);
 }
